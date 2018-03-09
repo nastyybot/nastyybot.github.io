66a73c9f4f29b2588d236b3ff1acd2036ee2d69a
ItsJustNasty <randomguyplaying@gmail.com>
2018-03-09 18:01:20 +0800

    Create login.js

---
+++ dev/login.js
@@ -0,0 +1,40 @@
+  var attempt = 3; //
+  function validate(){
+    var username = document.getElementById("username").value;
+    var password = document.getElementById("password").value;
+    if ( username == "nasty" && password == "admin1"){
+      var d = new Date();
+      d.setTime(d.getTime() + (exdays*24*60*60*1000));
+      var expires = "expires=" + d.toGMTString();
+      document.cookie = "username=ItsJustNasty" + expires;
+      alert ("Login successfully");
+      window.location = "dashboard.html"; 
+      return false;
+    } else if ( username == "pie" && password == "admin"){
+      var d = new Date();
+      d.setTime(d.getTime() + (exdays*24*60*60*1000));
+      var expires = "expires=" + d.toGMTString();
+      document.cookie = "username=TheFallingPie" + expires;
+      alert ("Login successfully");
+      window.location = "dashboard.html";
+      return false;
+    } else if ( username == "toni" && password == "admin"){
+      var d = new Date();
+      d.setTime(d.getTime() + (exdays*24*60*60*1000));
+      var expires = "expires=" + d.toGMTString();
+      document.cookie = "username=Toni" + expires;
+      alert ("Login successfully");
+      window.location = "dashboard.html";
+      return false;
+    } else{
+      attempt --;// Decrementing by one.
+      alert("You have left "+attempt+" attempt;");
+      // Disabling fields after 3 attempts.
+      if( attempt == 0){
+        document.getElementById("username").disabled = true;
+        document.getElementById("password").disabled = true;
+        document.getElementById("submit").disabled = true;
+        return false;
+      }
+  }
+}
