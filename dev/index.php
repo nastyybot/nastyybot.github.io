258d3ca7b8b9b353332a9bafcba16944ee7a5cb1
ItsJustNasty <randomguyplaying@gmail.com>
2018-03-09 17:36:22 +0800

    Update index.php

--- dev/index.php
+++ dev/index.php
@@ -1,14 +1,29 @@
-<?php
-
-
-?>
-
 <!DOCTYPE html>
 <html>
     <head>
-        <title>NASTY | DASHBOARD DEV</title>
+        <title>NASTY DASHBOARD DEV</title>
     </head>
+    
     <body>
-        <h1>My name jeff</h1>
+        <h2>Home</h2>
+        <?php
+            $has_Cookie_DisplayName = isset($_COOKIE["COOKIE_DisplayName"]);
+            if($has_Cookie_DisplayName == true){
+                $_cookie_DisplayName = $_COOKIE["COOKIE_DisplayName"];
+                echo "Welcome <strong>".$_cookie_DisplayName."!</strong> [<a href='dashboard.html'>DASHBOARD</a>] [<a href='logout.php'>LOGOUT</a>]";
+            } else {
+                if(isset($_SESSION) == false) {
+                    session_start();
+                }
+
+                $has_Cookie_DisplayName = isset($_SESSION["SESS_DISPLAYNAME"]);
+                if($has_Cookie_DisplayName == true){
+                    $session_DisplayName = $_SESSION['SESS_DISPLAYNAME'];
+                    echo "Welcome <strong>".$session_DisplayName."!</strong> [<a href='dashboard.html'>DASHBOARD</a>] [<a href='logout.php'>LOGOUT</a>];";
+                } else {
+                    echo "<a href='login.php'>Login</a>";
+                }
+            }
+        ?>
     </body>
 </html>
